12445e6e66852d4e3f2926e35ba713d2dfad2c77
Hans <hanwei@treefinance.com.cn>
2017-07-04 01:10:10 +0800

    add note

--- week.js
+++ week.js
@@ -1,15 +1,36 @@
 /**
  * Created by Hans on 2017/7/2.
+ *
  */
 var async = require('async');
+/**
+ * 1. your gitlab url
+ * 2. token (Profile Setting -> Account -> "Reset Private token")
+ * @type {ApiV3}
+ */
 var gitlab = require('gitlab')({
-    url: 'http://192.168.5.xxxx',
+    url: 'http://192.168.5.xxx',
     token: 'xxxxxx'
 });
 
 var markdown = require( "markdown" ).markdown;
 console.log(markdown.toHTML( "Hello *World*!" ));
 
+/**
+ * first.
+ *  look for your groups
+ */
+gitlab.groups.all(groups => {
+    groups.forEach(group =>{
+        console.log("groupId:"+group.id + "name:"+group.name)
+    });
+});
+
+/**
+ * second.
+ *  for example 203 is my groupId just from the result of the first step
+ *  yeah,u can replace 203 with your groupId
+ */
 async.waterfall([
     function (cb) {
         gitlab.groups.listProjects(203,(projects) => {
@@ -34,6 +55,10 @@ async.waterfall([
                 issues.forEach(issue =>{
                     var need = '';
                     var improve = '';
+                    /**
+                     * issue labels handle
+                     * @type {[*]}
+                     */
                     issue.labels = issue.labels.length == 0? ['其他']:issue.labels;
                     issue.labels.forEach(label => {
                         if(label.startsWith('待')){
@@ -43,12 +68,19 @@ async.waterfall([
                         }
 
                     })
+
                     console.log(check(issue.state)+issue.assignee.name+strong(need)+"issue#"+issue.iid+ ":"+issue.title+strong(improve));
                 })
             })
         })
     }
 ]);
+/**
+ * markdown str
+ *
+ * @param status
+ * @returns {*}
+ */
 var check = function(status){
     if (status ==='closed'){
         return '* [x] ';
@@ -62,6 +94,6 @@ var strong = function (content) {
     } else {
         return '';
     }
-}
+};
 
 
